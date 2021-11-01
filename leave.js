98b94756ba76632de55471bf44f83d1716a11f42
David <davidvdv229@gmail.com>
2021-11-01 07:15:42 +0200

    Lab4

--- leave.js
+++
@@ -1,5 +0,0 @@
-function leavePage() {
-    window.onbeforeunload = function() {
-        return "Sunteți siguri că doriți să treceți la o altă pagină?";
-    }
-}
--- personaje.js
+++
@@ -1,9 +0,0 @@
-function hiddenClickButton(id){
-    var el=document.getElementById(id);
-    if(el.style.display=="table-row"){
-        el.style.display="none";
-    } 
-    else {
-        el.style.display="table-row";
-    }
-}
